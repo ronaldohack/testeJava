27bdac22167dc195bc8b2bce9de170c9a6a03a6c
ronaldohack <contato_ronaldo@hotmail.com>
2020-09-11 12:21:01 -0300

    teste01com java

---
+++ src/ronaldo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ronaldo {
+    Scanner scan = new Scanner(System.in);
+    public static void main(final String[]args){
+        
+        String nome = "Ronaldo";
+        int idade = 20;
+       if(idade>=18){
+           System.out.println("ola " + nome + " seja bem vindo, você pode entrar sua idade é  "+idade);
+       }
+
+
+
+
+    }
+
+}
+
